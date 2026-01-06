fe07f4ecfc052cc8014d004462a73a229e386581
mahmoud.aldaas <mahmoud.aldaas@7p-group.com>
2026-01-06 14:25:42 +0100

    maybe ref or getter

--- src/composables/fetch.ts
+++ src/composables/fetch.ts
@@ -1,17 +1,20 @@
 import { ref } from 'vue'
 import { onMounted } from 'vue'
 import { readonly } from 'vue'
+import type { MaybeRefOrGetter } from 'vue'
+import { toValue } from 'vue'
+import { watchEffect } from 'vue'
 
-export function useFetch<T>(url: string) {
+export function useFetch<T>(url: MaybeRefOrGetter<string>) {
   const response = ref<T | null>(null)
   const error = ref<string | null>(null)
   const loading = ref(true)
 
-  onMounted(() => {
+  watchEffect(() => {
     response.value = undefined
     error.value = null
     loading.value = true
-    fetch(url)
+    fetch(toValue(url))
       .then((res) => res.json())
       .catch((err) => (error.value = err.message))
       .then((data) => (response.value = data))
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -2,16 +2,26 @@
 import type { PostsResponse } from '@/types.ts'
 import { useFetch } from '@/composables/fetch.ts'
 import AsyncResponse from '@/components/AsyncResponse.vue'
+import { ref } from 'vue'
+
+const page = ref(1)
 
 const {
   response: postsResponse,
   error,
   loading,
-} = useFetch<PostsResponse>('https://dummyjson.com/posts/?delay=1000')
+} = useFetch<PostsResponse>(
+  () => `https://dummyjson.com/posts/?limit=10&skip=${(page.value - 1) * 10}&delay=1000`,
+)
 </script>
 
 <template>
   <h1>Posts</h1>
+  <div>
+    <span v-for="i in 5" :key="i">
+      <button @click="page = i">{{ i }}</button>
+    </span>
+  </div>
   <AsyncResponse :response="postsResponse" :error="error" :loading="loading">
     <ul v-if="postsResponse?.posts.length">
       <li v-for="post in postsResponse?.posts" :key="post.id">
